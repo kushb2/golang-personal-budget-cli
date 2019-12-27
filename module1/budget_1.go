1a9e16180edda0cc9e3463effdc7ded34af50255
kushbhardwaj <kush.bhardwaj@nagarro.com>
2019-12-27 17:11:20 +0530

    'golang-personal-budget-cli'

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -2,12 +2,12 @@ package module1
 
 // Budget stores budget information
 type Budget struct {
-	max   float32
+	Max   float32
 	Items []Item
 }
 
 // Item stores item information
 type Item struct {
-	description string
-	price       float32
+	Description string
+	Price       float32
 }
